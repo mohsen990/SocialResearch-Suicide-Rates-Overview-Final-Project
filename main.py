5c7b58c27e5f8dc16909e25e13e5a46fe7961b81
mohsen990 <mohsen.saadatpour@gmail.com>
2024-04-25 17:35:27 +0200

    intoduction

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st
-import pandas as pd
-from settings import Path
+# Design main page with basic features of stream library
 
-st.write('Hello World!')
 
-df = pd.read_csv(Path)
-st.write(df.head(5))
+st.header('Suicide Rates Overview 1985 to 2016')
+
+
+st.image('https://english.mathrubhumi.com/image/contentid/policy:1.9160079:1702711234/uyf%20(1).jpg?$p=98c1d33&f=16x10&w=852&q=0.8', width=400)
+st.write("____________________________________________________________")
+st.subheader('Introduction:')
+st.markdown('''
+            The dataset comprises annual suicide counts in a specific country, categorized by gender and age groups, alongside the total population within each group. This setup enables the calculation of crude rates per 100,000 individuals.
+
+            While employing rates may seem intuitive since the total number of suicides correlates with population size (larger countries tend to have more suicides), it's less apparent that these rates, also known as crude rates, aren't suitable for cross-country comparisons. 
+            Suicide rates vary significantly based on demographic factors. Thus, to assess a country's situation accurately, it's imperative to consider its current demographic makeup. 
+            
+            Overall, the dataset appears satisfactory, albeit with some missing values. To enhance clarity, I'll perform two transformations to organize and present age categories more effectively.
+            ''')
--- pages/1-Data Exploration.py
+++ pages/1-Data Exploration.py
@@ -1,96 +1,44 @@
+import pandas as pd
 import streamlit as st
-from settings import Path , AboutDataSet , KeyFeaturesInDataSet
-import GetDataFrame as gf
-#-------------------------------------------------------------------------
+from settings import Path
+
+AboutDataSet = '''
+            This compiled dataset pulled from four other datasets linked by time and place, and was built to find signals correlated to
+            increased suicide rates among different cohorts globally, across the socio-economic spectrum.
+            '''
+
+KeyFeaturesInDataSet = '''
+              
+            :green[Country:] Country Name
+            
+            :green[Year:] The year of suicide information collected 
+            
+            :green[Sex:] The gender of the person who committed suicide 
+
+            :green[Age:] Age category of suicidal person
+
+            :green[suicide NO:] Count of suicides
+
+            :green[Population:] Population of the country in the corresponding year
+
+            :green[suicide rate:] Suicides per 100000 people 
+
+            :green[country-year:] The Country and Year (key)
+
+            :green[HDI for year:] Human Development Index / per year
+
+            :green[GDP for year:] Gross Domestic Product / per year
+
+            :green[Generation:] generation type baed on 
+
+            '''
+
+
 
 
 st.subheader("About Dataset:")
-st.markdown( AboutDataSet )
-
-with st.expander("See Key Features:"):
-   st.markdown( KeyFeaturesInDataSet )
-   
-
-# get orignal Dataset and uncleaned -----------------------------------------
-
-df = gf.GetUncleanData()
-
-# Disply data frame----------------------------------------------------------
-
-st.write('Shape of Dataset:', df.shape)
-st.markdown('Dataset Descrition:')
-DataSetDescribe = df.describe(include='all')
-st.write(DataSetDescribe)
-
-st.subheader("Display Data set: " )
-SelectedItem = st.selectbox("Select number of rows to show: " , ["select number","10","100","1000","All"])
-if SelectedItem == "10":
-   st.write(df.head(10))
-elif SelectedItem == "100":
-   st.write(df.head(100))
-elif SelectedItem == "1000":
-   st.write(df.head(1000))
-elif SelectedItem == "All":
-    st.write(df)
-else:
-   st.write(df.head())
-
-
-# Data Cleaning ------------------------------------------------------------
-
-st.subheader('Data Cleaning:')
-st.write('''
-            Data cleaning includes data casting , converting into integer in some columns,
-            ,extracting numbers from strings, removing rows with low number of null values
-            and replacing null cells with mean of value in cloumns with large number of null values.
-         ''')
-
-# display count of null valuses in each column
-sumNa = df.isna().sum()
-with st.expander("Show number of null values in each columns before cleaning"):
-     strs = ""
-     for item in sumNa.index:
-         strs = strs +'\t\t' +item + ' : :red[' + str(sumNa[item]) +']'
-     st.write(strs)
-
-# get Clean Datafarme and check null values-----------------------------------
-CleanDataFrame = gf.GetCleanData()
-sumNa = CleanDataFrame.isna().sum()
-with st.expander("Show number of null values in each columns after cleaning"):
-     strs = ""
-     for item in sumNa.index:
-         strs = strs +'\t\t' +item + ' : :red[' + str(sumNa[item]) +']'
-     st.write(strs)
-
-     
-#Filter Data to dispaly---------------------------------------------------------
-CleanDataFrame['Year'] = CleanDataFrame['Year'].astype(str)   
-
-ListOfBrands = ["Select Brand"]
-ListOfBrands.extend(CleanDataFrame.Brand.drop_duplicates().tolist())
-SelectedBrand = st.selectbox("Filter By Brand" ,ListOfBrands )
-if SelectedBrand == "Select Brand":
-    FiltterdDf = ""
-else:
-   FiltterdDf =  CleanDataFrame[ CleanDataFrame.Brand == SelectedBrand ]
-st.write(FiltterdDf)
-
-
-ListOfBody = ["Select BodyType"]
-ListOfBody.extend(CleanDataFrame.BodyType.drop_duplicates().tolist())
-SelectedBody = st.selectbox("Filter By Body Type" ,ListOfBody )
-if SelectedBody == "Select BodyType":
-    FiltterdDf2 = ""
-else:
-   FiltterdDf2 =  CleanDataFrame[ CleanDataFrame.BodyType == SelectedBody ]
-st.write(FiltterdDf2)
-
-#-- Group by ------------------------------------------------------------------------------
-st.subheader('Group By Features')
-st.write('Group by with Brand and Model:')
-df_gr1 = CleanDataFrame.groupby(['Brand','Model'])
-st.write(df_gr1.first())
-
-st.write('Total Price based on brand, model and type:')
-df_gr2 = CleanDataFrame.groupby(['Brand','Model','BodyType'])['Price'].sum()
-st.write(df_gr2)
+st.markdown(AboutDataSet)
+st.write('________________________________________________________')
+st.subheader("KeyFeaturesInDataSet:")
+st.markdown(KeyFeaturesInDataSet)
+st.write('________________________________________________________')
